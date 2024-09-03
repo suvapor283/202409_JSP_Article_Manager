af58249dbcc368bb36ed3e3af0d759796c8eeabb
suvapor283 <suvapor8632@gmail.com>
2024-09-03 21:37:49 +0900

    JSP Article Manager 시작

---
+++ src/main/java/com/koreaIT/jsp/am/HomeMainServlet.java
@@ -0,0 +1,20 @@
+package com.koreaIT.jsp.am;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/home/main")
+public class HomeMainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8;");
+		response.getWriter().append("<div style=\"color:red;\">한글</div>");
+		response.getWriter().append("한글");
+	}
+}
